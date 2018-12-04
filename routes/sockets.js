081be5e8236dd995bcae78ccc0b11b3e20bb2b70
Yehonatan Barr <jonybh99@gmail.com>
2018-12-04 19:47:18 +0200

    notification works

--- routes/sockets.js
+++ routes/sockets.js
@@ -2,21 +2,29 @@ const chatService = require('../services/chat-service')
 
 var gChats = chatService.query()
 function connectSockets(io) {
-    
+
     io.on('connection', function (socket) {
         console.log('a user connected')
-        var chatToUpdate
         socket.on('sendMsg', function (msg, chatId) {
             chatService.getById(chatId)
                 .then(chat => {
                     chat.msgs.push(msg)
                     socket.join('chatRoom' + chatId)
-                    io.to('chatRoom' + chatId).emit('sentMsg', {msg, chatId})
+                    io.to('chatRoom' + chatId).emit('sentMsg', { msg, chatId })
                     chatService.update(chat)
                 })
 
-            
+
         });
+
+        socket.on('newUserSocket', function (userId) {
+            console.log('opening new socket for:', userId)
+            socket.join('userSocket' + userId)
+        })
+        socket.on('emitToUser', function (eventMsg, userId) {
+            io.to('userSocket'+userId).emit('eventMsgToUser', eventMsg)
+        })
+
         socket.on('disconnect', function () {
             console.log('user disconnected')
         });
